e23bcd92fc7a099a7a5532f41ae768eaa48042fc
ken2063 <ken2063@naver.com>
2024-02-16 16:22:46 +0900

    1

---
+++ mobile/assets/Common_M00_p101-5ea12fea.js
@@ -0,0 +1,1 @@
+import{I as o}from"./IllustObject-2df52952.js";import{_ as e,Q as s,q as c,g as n}from"./index-cff3a931.js";const p={components:{IllustObject:o}};function r(a,_,l,m,f,u){const t=s("IllustObject");return n(),c(t,{type:"capital"})}const I=e(p,[["render",r]]);export{I as default};
