f1b0a76fddc61890695a18672dfd730e870ca72a
ken2063 <ken2063@naver.com>
2024-02-16 15:15:48 +0900

    1

---
+++ mobile/assets/SplashPage-41449b72.js
@@ -0,0 +1,1 @@
+import{I as e}from"./IllustObject-2115e3ff.js";import{_ as o,Q as s,q as c,g as n}from"./index-ce6f4fae.js";const a={components:{IllustObject:e}};function p(r,l,_,m,f,u){const t=s("IllustObject");return n(),c(t,{type:"capital"})}const I=o(a,[["render",p]]);export{I as default};
