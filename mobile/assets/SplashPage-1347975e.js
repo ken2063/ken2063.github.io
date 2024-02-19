560ae430dc5c6944ba3ceac964c6a405e010dcaa
ken2063 <ken2063@naver.com>
2024-02-19 13:36:40 +0900

    4

---
+++ mobile/assets/SplashPage-1347975e.js
@@ -0,0 +1,1 @@
+import{I as e}from"./IllustObject-852e718e.js";import{_ as o,Q as s,q as c,g as n}from"./index-d7ec5595.js";const a={components:{IllustObject:e}};function p(r,l,_,m,f,u){const t=s("IllustObject");return n(),c(t,{type:"capital"})}const I=o(a,[["render",p]]);export{I as default};
---
+++ mobile/assets/box-detail-724b813c.js
@@ -0,0 +1,1 @@
+const m="/mobile/images/_dummy/box-detail.png";export{m as _};
