b44616df55b4b4b5308c4b24565feed6d1035660
seok <edseok88@gmail.com>
2025-08-04 13:49:42 +0900

    수정

--- vue-project/vite.config.js
+++ vue-project/vite.config.js
@@ -1,14 +1,22 @@
+// vite.config.js
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import * as path from "path";
 
 export default defineConfig({
-  base: "/", // ← 저장소 이름에 맞게 수정!
+  base: "/", // 저장소 이름에 맞게 수정!
   plugins: [vue()],
   resolve: {
     alias: { "@": path.resolve(__dirname, "./src") },
   },
   build: {
-    outDir: "../", // 기본값, dist 폴더에 빌드 결과 생성
+    outDir: "../update", // (추천) 루트 내부의 'dist' 폴더로 지정
+  },
+  server: {
+    port: 8001,
+    host: 'localhost',
+    watch: {
+      usePolling: true,
+    }
   },
 });
